355d820de6f13cf92aec815b5cc656d3fc9c52ed
Aleks_D <Duraj94@gmail.com>
2016-11-28 13:55:58 +0100

    add funtion calculateEmployeeSalary

--- Company/ProjectCompany/ProjectCompany/Main.cpp
+++ Company/ProjectCompany/ProjectCompany/Main.cpp
@@ -27,15 +27,33 @@ string findEmloyeeWithLongestName(const string name[], size_t size)
     return longestName;
 }
 
+int calculateEmployeeSalary(int year, const int hireYear[], const int hireMonth[], const double salary[], size_t n)
+{
+    if (hireYear[n] < year)
+        return (salary[n] * 12);
+    if (hireYear[n] == year)
+        return ((12 - hireMonth[n]) * salary[n]);
+    return 0;
+}
+
 int main()
 {
-    string id[] = { "E001", "E002", "E003" };
-    string name[] = { "Ola", "Basia", "Mateusz" };
+    string id[] = { "E001", "E002", "E003" },
+           name[] = { "Ola", "Basia", "Mateusz" };
     double salary[] = { 3500, 3750, 3600 };
-    size_t size = 3;
+    int hireYear[] = { 2015, 2016, 2016 },
+        hireMonth[] = { 7, 3, 8 };
+    size_t size = 3,
+           employeeNumber = 1,
+           year = 2016;
+
 
     printEmployee(id, name, salary, size);
     cout << "Longest name is " << findEmloyeeWithLongestName(name, size) << endl;
+    cout << "Employee " << name[employeeNumber]
+         << " in "<< year
+         << " salary: " << calculateEmployeeSalary(year, hireMonth, hireYear, salary, employeeNumber) << endl;
+
 
     system("pause");
     return 0;
