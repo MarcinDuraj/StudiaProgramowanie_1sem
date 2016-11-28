77413bf66b93c4bf88811173558bed30cfd6db83
Aleks_D <Duraj94@gmail.com>
2016-11-28 13:17:43 +0100

    Add function printEmployee

--- Company/ProjectCompany/ProjectCompany/Main.cpp
+++ Company/ProjectCompany/ProjectCompany/Main.cpp
@@ -3,13 +3,27 @@
 
 using namespace std;
 
+void printEmployee(const string id[], const string name[], const double salary[], size_t size)
+{
+    cout << "------------------------" << endl;
+    for (size_t i = 0; i < size; ++i)
+    {
+        cout << "Id:\t" << id[i] << endl
+            << "Name:\t" << name[i] << endl
+            << "Salary:\t" << salary[i] << endl
+            << "------------------------" << endl;
+    }
+    return;
+}
+
 int main()
 {
-
     string id[] = { "E001", "E002", "E003" };
     string name[] = { "Ola", "Basia", "Mateusz" };
     double salary[] = { 3500, 3750, 3600 };
     size_t size = 3;
+
+    printEmployee(id, name, salary, size);
 
     system("pause");
     return 0;
