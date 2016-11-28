788757348a04d1c5beaf7bc1d7a0dada46f3e127
Aleks_D <Duraj94@gmail.com>
2016-11-28 13:03:05 +0100

    Company project main file

---
+++ Company/ProjectCompany/ProjectCompany/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    string id[] = { "E001", "E002", "E003" };
+    string name[] = { "Ola", "Basia", "Mateusz" };
+    double salary[] = { 3500, 3750, 3600 };
+    size_t size = 3;
+
+    system("pause");
+    return 0;
+}
