20fb086fed9210f2017927beffe6839dfe534af3
MarcinDuraj <DurajMarcin42@gmail.com>
2016-12-07 13:53:43 +0100

    add task 2 , 3 add overloaded function boolToString

--- Zadania/Zadania/main.cpp
+++ Zadania/Zadania/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-bool checkNumberPartition(const int number, const int min, const int max)
+bool checkNumberInPartition(const int number, const int min, const int max)
 {
     if (number > min && number < max)
         return true;
@@ -11,30 +11,50 @@ bool checkNumberPartition(const int number, const int min, const int max)
 
 string boolToString(bool x)
 {
-    if (x == false)
-        return "NIE";
-    return "TAK";
+    if (x == true)
+        return "TAK";
+    return "NIE";
+}
+
+string boolToString(bool x, bool y)
+{
+    if (x == true && y == true)
+        return "TAK";
+    return "NIE";
 }
 
 int main()
 {
-    int requestChoice;
+    int requestChoice,
+        x,
+        y;
     string str;
 
     do
     {   
 
         cout << "1.  Sprawdzic czy liczba x wprowadzona z klawiatury miesci sie w przedziale [10,20) " << endl
+             << "2. Sprawdzic czy x nalezy do (−20,5] i y nalezy do [15,100]" << endl
+             << "3. Sprawdzic czy x nalezy (20,40) lub y nalezy [23,27) " << endl
              << "0. Koniec zadania" << endl;
         cout << "Podaj numer: ";
         cin >> requestChoice;
         switch (requestChoice)
         {
         case 1 :
-            int x;
             cout << "Podaj x: ";
             cin >> x;
-            cout << "Czy x miesci sie w przedziale: " << boolToString(checkNumberPartition(x, 9, 20)) << endl;
+            cout << "Czy x miesci sie w przedziale: " << boolToString(checkNumberInPartition(x, 9, 20)) << endl;
+            break;
+        case 2:
+            cout << "Podaj x, y: ";
+            cin >> x >> y;
+            cout << "Czy x oraz y miesci sie w przedziale: " << boolToString(checkNumberInPartition(x, -20, 6), checkNumberInPartition(y, 14, 101)) << endl;
+            break;
+        case 3:
+            cout << "Podaj x, y: ";
+            cin >> x >> y;
+            cout << "Czy x lub y miesci sie w przedziale: " << boolToString(checkNumberInPartition(x, 20, 40) - checkNumberInPartition(y, 22, 27)) << endl;
             break;
         case 0 : 
             break;
